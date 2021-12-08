1d3e80a93dd2c9e5d01b75d78bf65b0ec16ca44c
RaphBhz <raphael.bouchez@etu.u-paris.fr>
2021-12-09 00:15:03 +0100

    solve_knapsack_optimal and solve_knapsack_optimal_recursive
    Creation of the "optimal" solution
    inspired by :
    https://www.educative.io/blog/0-1-knapsack-problem-dynamic-solution

--- solutions.py
+++ solutions.py
@@ -3,7 +3,7 @@ from knapsack.Knapsack import Knapsack
 
 def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
     assert(knapsack.content == [])
-    filled_sack = knapsack
+    filled_sack = knapsack.copy()
 
     sorted_objects_dict = dict(sorted(objects_dict.items(), key=lambda x: x[1][0]/x[1][1], reverse=True))
     weight = 0
@@ -21,5 +21,26 @@ def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
 
 
 def solve_knapsack_optimal(knapsack, objects_dict) -> Knapsack:
-    pass
+    knapsack_optimal = knapsack.copy()
+    objects_dict_iterated = objects_dict.copy()
+    objects_dict_iterated = list(objects_dict_iterated.items())
 
+    return solve_knapsack_optimal_recursive(knapsack_optimal, objects_dict, objects_dict_iterated, 0)
+
+
+def solve_knapsack_optimal_recursive(knapsack, objects_dict, objects_list, i) -> Knapsack:
+    if i >= len(objects_list):
+        return knapsack
+
+    sack_left = knapsack.copy()
+    sack_right = knapsack.copy()
+
+    if knapsack.get_value_and_weight(objects_dict)[1] + objects_list[i][1][1] <= knapsack.capacity:
+        sack_left.content.append(objects_list[i][0])
+        sack_left = solve_knapsack_optimal_recursive(sack_left, objects_dict, objects_list, i + 1)
+
+    sack_right = solve_knapsack_optimal_recursive(sack_right, objects_dict, objects_list, i + 1)
+
+    if sack_left.get_value_and_weight(objects_dict)[0] > sack_right.get_value_and_weight(objects_dict)[0]:
+        return sack_left
+    return sack_right
